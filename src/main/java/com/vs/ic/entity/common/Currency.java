c1386fe9690208c4ae326881a23725c36d00b35f
Ripon Rana <ripon.rana@bracits.com>
2016-08-10 17:31:00 +0600

    New Classes Added

---
+++ src/main/java/com/vs/ic/entity/common/Currency.java
@@ -0,0 +1,8 @@
+package com.vs.ic.entity.common;
+
+public class Currency {
+	Long id;
+	String name;
+	String symbol;
+	Boolean isLocal;
+}
---
+++ src/main/java/com/vs/ic/entity/common/MeasurementUnit.java
@@ -0,0 +1,5 @@
+package com.vs.ic.entity.common;
+
+public class MeasurementUnit {
+
+}
